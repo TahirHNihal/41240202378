d4e17c76f277bcbcfe22a8a214619d13115922e3
TahirHNihal <tahirhnihal29@gmail.com>
2024-11-06 19:05:38 +0600

    first commit

---
+++ Problem 1/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int intNum = 10;
+    float fltNum = 3.1416;
+    char charVar = 'c';
+
+    printf("The integer value: %d", intNum);
+    printf("\nThe floating point value: %f", fltNum);
+    printf("\nThe character value: %c", charVar);
+
+    return 0;
+}
---
+++ Problem 1/6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter your age in years: ");
+    scanf("%d", &age);
+
+    printf("Your age is: %d years\n", age);
+
+    return 0;
+}
