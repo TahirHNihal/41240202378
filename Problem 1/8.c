491961e50cfaaef43ecab7bed11eae9ea04607ee
TahirHNihal <tahirhnihal29@gmail.com>
2024-11-06 20:27:58 +0600

    problem 1.8 and 1.9 solved

---
+++ Problem 1/8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &num1);
+
+    printf("Enter 2nd number: ");
+    scanf("%d", &num2);
+
+    printf("Enter 3rd number: ");
+    scanf("%d", &num3);
+
+    printf("First Value = %d, ", num1);
+    printf("Last Value = %d", num3);
+    return 0;
+}
---
+++ Problem 1/9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    double dlVar = 3.1416;
+    bool booleanVar = true;
+
+    printf("%f\n", dlVar);
+    printf("%d\n", booleanVar);
+
+    return 0;
+}
