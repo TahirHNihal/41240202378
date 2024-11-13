f2ace0abae80a6e52559391537e6f44a7b54756a
Tahir Hossain Nihal <71434221+TahirHNihal@users.noreply.github.com>
2024-11-13 09:26:07 +0600

    Update 8.c

--- Problem 1/8.c
+++ Problem 1/8.c
@@ -3,16 +3,11 @@ int main()
 {
     int num1, num2, num3;
 
-    printf("Enter 1st number: ");
-    scanf("%d", &num1);
-
-    printf("Enter 2nd number: ");
-    scanf("%d", &num2);
-
-    printf("Enter 3rd number: ");
-    scanf("%d", &num3);
+    printf("Enter 3 numbers : ");
+    scanf("%d %d %d", &num1, &num2, &num3);
 
     printf("First Value = %d, ", num1);
     printf("Last Value = %d", num3);
+    
     return 0;
 }
